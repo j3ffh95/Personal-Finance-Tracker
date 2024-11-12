29c56cd4f0df4c27cadac7eb0f7f92febc7b6c6c
Jeff <j3ffh95@gmail.com>
2024-11-11 23:13:30 -0500

    Created the function that adds a new entry

--- data_entry.py
+++ data_entry.py
@@ -12,5 +12,21 @@ class CSV:
         try:
             pd.read_csv(cls.CSV_FILE)
         except FileNotFoundError:
+            # A DataFrame is an object within pandas that allows us to access different rows/columns from a CSV file
             df = pd.DataFrame(
                 columns=["date", "amount", "category", "description"])
+            # The code below will write the DataFrame to the CSV file
+            df.to_csv(cls.CSV_FILE, index=False)
+
+    # Add some entries to the CSV file
+    @classmethod
+    def add_entry(cls, amount, category, description):
+        new_entry = {
+            "date": date,
+            "amount": amount,
+            "category": category,
+            "description": description
+        }
+
+
+CSV.initialize_csv()
