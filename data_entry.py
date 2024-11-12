10d20bc68a082d7c6a80850502961762f309bbe1
Jeff <j3ffh95@gmail.com>
2024-11-11 23:29:17 -0500

    Added different function we will need to get user input

---
+++ data_entry.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+
+def get_date(prompt, allow_default=False):
+    pass
+
+
+def get_amount(prompt):
+    pass
+
+
+def get_category(prompt):
+    pass
+
+
+def get_description(prompt):
+    pass
