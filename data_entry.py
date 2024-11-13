a022114dd0b0d03f3f72495fa8119efa373f9c97
Jeff <j3ffh95@gmail.com>
2024-11-12 21:45:59 -0500

    Added a try block statement to check for the date of the user input

--- data_entry.py
+++ data_entry.py
@@ -2,7 +2,12 @@ from datetime import datetime
 
 
 def get_date(prompt, allow_default=False):
-    pass
+    date_str = input(prompt)
+    if allow_default and not date_str:
+        return datetime.today().strftime("%m-%d-%Y")
+    try:
+        valid_date = datetime.strptime(date_str, "%m-%d-%Y")
+        return valid_date.strftime("%m-%d-%Y")
 
 
 def get_amount(prompt):
