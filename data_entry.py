8b8a4008b4788476d8cb92722a59bbd20e8c8753
Jeff <j3ffh95@gmail.com>
2024-11-12 21:49:31 -0500

    Finished the get_date function

--- data_entry.py
+++ data_entry.py
@@ -1,13 +1,18 @@
 from datetime import datetime
 
+date_format = "%m-%d-%Y"
+
 
 def get_date(prompt, allow_default=False):
     date_str = input(prompt)
     if allow_default and not date_str:
-        return datetime.today().strftime("%m-%d-%Y")
+        return datetime.today().strftime(date_format)
     try:
-        valid_date = datetime.strptime(date_str, "%m-%d-%Y")
-        return valid_date.strftime("%m-%d-%Y")
+        valid_date = datetime.strptime(date_str, date_format)
+        return valid_date.strftime(date_format)
+    except ValueError:
+        print("Invalid date format. Please enter the date in mm-dd-yyyy format.")
+        return get_date(prompt, allow_default)
 
 
 def get_amount(prompt):
